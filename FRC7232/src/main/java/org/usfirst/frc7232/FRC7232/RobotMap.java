570c4308ee1761be61bec8bf9632f108f8aff7d9
7232CMA <56237429+7232CMA@users.noreply.github.com>
2020-02-09 21:33:06 -0500

    FRC7232 Project
    2020 Initial Commit

---
+++ FRC7232/src/main/java/org/usfirst/frc7232/FRC7232/RobotMap.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc7232.FRC7232;
+
+import edu.wpi.first.wpilibj.Talon;
+
+/**
+ * Define all the ports used in the robot here 
+ * Motor ports 
+ * Joystick ports
+ */
+public class RobotMap {
+    //Talon Motor Ports Defined 
+    public static int DRIVETRAIN_LEFT_FRONT_Talon = 0;
+    public static int DRIVETRAIN_LEFT_REAR_Talon = 1;
+    public static int DRIVETRAIN_RIGHT_FRONT_Talon = 2;
+    public static int DRIVETRAIN_RIGHT_REAR_Talon = 3;
+
+    //JoyStick Ports 
+    public static int JOYSTICK_DRIVE_CONTROLLER = 0;
+    public static int JOYSTICK_GAME_CONTROLLER = 1;
+
+
+    //Button Ports 
+
+
+}
